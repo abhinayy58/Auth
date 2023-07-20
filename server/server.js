7d1da52a3700041220629a15b6c8f87a39f18418
abhinayy58 <abhinayy48@gmail.com>
2023-07-20 20:49:43 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cookieParser from 'cookie-parser'
+dotenv.config()
+import connectDB from './config/db.js';
+import { notFound,errorHandler } from './middleware/ErrorMiddleware.js';
+const port = process.env.PORT || 5000;
+import userRoutes from './routes/userRoutes.js'
+const app = express();
+  
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+connectDB()
+app.use('/api/users',userRoutes)
+
+app.get("/", (req, res) => res.send("Server is Ready"));
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, () => {
+    console.log('listening on port', port);
+})
+
