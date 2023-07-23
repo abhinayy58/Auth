992b18d50a8bff5bd0cd8eeb338cd41094b7f804
abhinayy58 <abhinayy48@gmail.com>
2023-07-23 18:48:19 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -3,11 +3,16 @@ import express from 'express';
 import cookieParser from 'cookie-parser'
 dotenv.config()
 import connectDB from './config/db.js';
+import cors from 'cors';
 import { notFound,errorHandler } from './middleware/ErrorMiddleware.js';
 const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js'
 const app = express();
   
+
+app.use(cors({
+    origin:'http://localhost:3000'
+}))
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -21,5 +26,5 @@ app.use(errorHandler)
 
 app.listen(port, () => {
     console.log('listening on port', port);
-})
+}) 
 
